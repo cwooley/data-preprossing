ee9560df2c2f55e8041a4a63339fb57048b57b24
Charles Wooley <cwooley89@gmail.com>
2017-10-23 12:44:17 -0400

    initial commit of data preprosessing repo

---
+++ python_import.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 11:37:21 2017
+
+@author: Charles
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+
+
